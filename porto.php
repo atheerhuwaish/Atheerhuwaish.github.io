cce654c0ce1cc564cdd95cfb3d6e179c0aa7856f
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2018-07-08 23:15:34 +0200

    Add files via upload

---
+++ porto.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+	<main>
+<h3>Porto</h3>
+<p>Porto ist die zweitgrößte Stadt Portugals. Die Stadt, deren Name im Deutschen wörtlich Hafen heißt, liegt an der Atlantikküste am Nordufer des dort mündenden Douro. Sie hat 238.000 Einwohner und ist Hauptstadt des gleichnamigen Distriktes. Mit der gleichnamigen Metropolregion, in der 1,76 Millionen Menschen leben, bildet sie das wirtschaftliche und kulturelle Zentrum Nordportugals.</p>
+	
+<img src="bilder_porto/porto1.jpg" alt="porto1">
+<img src="bilder_porto/porto2.jpg" alt="porto2">
+<img src="bilder_porto/porto3.jpg" alt="porto3">
+	</main>
+	<?php include 'footer.php'; ?>
