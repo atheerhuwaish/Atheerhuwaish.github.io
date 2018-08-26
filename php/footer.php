4da1ca68caa7d935927a088974371978ba23576a
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2018-08-26 11:46:41 +0200

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -1,23 +1,23 @@
-		
-		<aside id="aside2">
-			<ul>
-                <li><b>CSS Links</b>
-				    <ul>
-                        <li><a href="https://css-tricks.com/" target="_blank">CSS-                  Tricks</a></li>
-					           <li><a                                                       href="https://www.w3schools.com/css/default.asp"       target="_blank">W3schools - CSS
-                                   </a>
-                               </li>
-					           <li>
-                                   <a href="http://css3buttongenerator.com/" target="_blank">CSS Button Generator
-                                   </a>
-                               </li>
-				    </ul>
-				</li>
-			</ul>
-		</aside>
+
+	<aside class="aside2">
+		<ul><li><b><a href="#">Aside 2</a></b>
+				<ul><li><a href="https://css-tricks.com" 
+						target="_blank">CSS-Tricks</a>
+					</li>
+					<li><a href="https://www.w3schools.com/css/default.asp" 
+						target="_blank">W3schools - CSS</a>
+					</li>
+					<li><a href="http://css3buttongenerator.com" 
+						target="_blank">CSS Button Generator</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</aside>
+	
 		
 	<footer>
-		<p>Abschlussprojekt - alfatraining</p>
+		<p>Atheer Huwaish &copy;</p>
 	</footer>
 	
 	</div> <!--Ende container-->
