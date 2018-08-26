ff468bd29c07482b168fb0ba49aaa32921a72085
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2018-08-26 11:48:08 +0200

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,13 +1,39 @@
 <?php include 'header.php'; ?>
+	
 <main>
-<h2>Dies ist eine private Website.<br>
-Ich speichere und nutze Ihre Daten nicht.<br>
-Das Formular funktioniert nicht und kann keine Daten senden.<br>
-<br><br>
-Atheer Huwaish<br><br>
+<h3>Info</h3><br>
+
+<h4>Impressum</h4><br>
+		
+Angaben gemäß § 5 TMG<br>
+Atheer Huwaish <br> 
+Markstr. 364<br> 
+44795 Bochum <br><br>
+
+Vertreten durch:<br>
+Atheer Huwaish<br>
+Telefon: 015775191967<br>
+E-Mail: <a href="mailto:atheerh@web.de" target="_blank" rel="nofollow">atheerh@web.de</a></br><br>
+
+<h4>Datenschutz</h4><br>
+
+Die Nutzung dieser Webseite erfolgt ohne Angabe personenbezogener Daten.
+Sie ist eine statische Webseite und ich erhalte keine Daten über die Besucher. Das Formular hat keine Funktion und kann keine Daten senden.<br>
+E-mails an uns werden so schnell wie möglich gelöscht, wenn die nicht mehr für den mit Ihnen vereinbarten Zweck erforderlich ist.<br>
+
+Personenbezogene Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.<br>
 
 Webhosting Provider:<br>	
-Github <a href="https://help.github.com/articles/github-privacy-statement" target="_blank">Privacy Statement</a></h2><br><br>
-</main>
+Github <a href="https://help.github.com/articles/github-privacy-statement" target="_blank">Privacy Statement</a><br><br>
+
+<h4>2-spaltiges Template</h4><br>
+
+Dies ist ein responsives Template, das ab einer Breite von 1025px 3-spaltig wird<br>
+
+Ein responsives 2-spaltiges Template finden Sie hier:<br>
+<a href="https://atheerhuwaish.github.io" target="_blank" rel="nofollow">2-spaltiges responsives Template</a>.<br><br>
+<a href="https://github.com/atheerhuwaish" target="_blank" rel="nofollow">Meine Seite auf Github</a>.<br><br>
+
+	</main>
 	
 	<?php include 'footer.php'; ?>
