2951933d39b7a3dcd7b752c2e0d41e21be871148
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2019-04-27 23:25:17 +0200

    Update info.php

--- php/info.php
+++ php/info.php
@@ -3,13 +3,13 @@
 <main>
 <h3>Info</h3><br>
 
-<img class="info" src="../atheer_photo.jpg" alt="personal photo"><br>
-<h4 class="left">Responsives Template (2-spaltig)</h4><br>
+<img class="info" src="../atheer_photo.jpg" alt="personal photo">
+<h4 class="left">Responsives Template (2-spaltig)</h4>
 <a class="left" href="https://atheerhuwaish.github.io" target="_blank" rel="nofollow">2-spaltiges responsives Template (2-column responsive template)</a>.<br><br>
 
 <a class="left" href="https://github.com/atheerhuwaish" target="_blank" rel="nofollow">Mein Github-Konto (my account on Github)</a>.<br><br>
 
-<h4 class="left">Impressum</h4><br>
+<h4 class="left">Impressum</h4>
 		
 Angaben gemäß § 5 TMG<br>
 Atheer Huwaish <br> 
@@ -21,7 +21,7 @@ Atheer Huwaish<br>
 Telefon: 015775191967<br>
 E-Mail: <a href="mailto:atheerh@web.de" target="_blank" rel="nofollow">atheerh@web.de</a></br><br>
 
-<h4 class="left">Datenschutz</h4><br>
+<h4 class="left">Datenschutz</h4>
 
 Die Nutzung dieser Webseite erfolgt ohne Angabe personenbezogener Daten.
 Sie ist eine statische Webseite und ich erhalte keine Daten über die Besucher. Das Formular hat keine Funktion und kann keine Daten senden.<br>
