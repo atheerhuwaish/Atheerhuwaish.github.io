de60158dbb0584740b13181ecd6793412bf1ebbd
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2019-05-25 18:47:11 +0200

    Update bilder.php

--- php/bilder.php
+++ php/bilder.php
@@ -5,18 +5,18 @@
 
 		<h3>Lissabon</h3><br>
 
-		<img src="bilder/lissabon1.JPG" alt="lisbon1">
-		<img src="bilder/lissabon2.JPG" alt="lisbon2">
-		<img src="bilder/lissabon3.JPG" alt="lisbon3">
-		<img src="bilder/lissabon4.JPG" alt="lisbon5">
-		<img src="bilder/lissabon5.JPG" alt="lisbon6" class="rotate90"><br><br><br><br>
+		<img src="../lissabon1.JPG" alt="lisbon1">
+		<img src="../lissabon2.JPG" alt="lisbon2">
+		<img src="../lissabon3.JPG" alt="lisbon3">
+		<img src="../lissabon4.JPG" alt="lisbon5">
+		<img src="../lissabon5.JPG" alt="lisbon6" class="rotate90"><br><br><br><br>
 		<!-- delete  class="rotate90" if the image will not be diplayed properly -->
 		
 		<h3>Porto</h3><br>
-		<img src="bilder/porto1.JPG" alt="porto1" class="rotate180">
+		<img src="../porto1.JPG" alt="porto1" class="rotate180">
 		<!-- delete  class="rotate180" if the image will not be diplayed properly -->
-		<img src="bilder/porto2.JPG" alt="porto2">
-		<img src="bilder/porto3.JPG" alt="porto3">
+		<img src="../porto2.JPG" alt="porto2">
+		<img src="../porto3.JPG" alt="porto3">
 	</main>
 	
 	<?php include 'footer.php'; ?>
