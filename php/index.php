74323293be8617871f633ddeb59b4dabb126fd58
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2019-05-25 18:45:49 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,16 +1,41 @@
 <?php include 'header.php'; ?>
 
-	<main>
-<h3>Portugal</h3>
-<p>Die Republik Portugal (amtlich República Portuguesa) ist ein europäischer Staat im Westen der Iberischen Halbinsel. Als westlichster Punkt Kontinentaleuropas wird das Land im Osten und Norden von Spanien begrenzt, im Westen und Süden dagegen vom Atlantischen Ozean. Zum portugiesischen Staatsgebiet gehören die Inseln der Azoren und Madeira (mit Porto Santo).</p>
+	<main id="main">
 
-<p>Das im 12. Jahrhundert gegründete Königreich Portugal stieg im 15. Jahrhundert zu einer Großmacht auf und spielte eine wesentliche Rolle im Zeitalter der Entdeckungen. Das Königreich schuf eines der größten Kolonialreiche mit Besitzungen in Afrika, Asien und Südamerika, dessen Niedergang jedoch bereits im Laufe des 17. Jahrhunderts eingeläutet wurde. 1910 kam es durch einen militärischen Aufstand zum Sturz der portugiesischen Monarchie, der König Manuel II. ins Exil zwang.</p>
-        
-<p>Die Erste Portugiesische Republik trat am 5. Oktober 1910 in Kraft und bestand bis zum Militärputsch von General Gomes da Costa im Jahr 1926. Danach stand das Land für mehr als vierzig Jahre unter der autoritären Diktatur von António de Oliveira Salazar. Die Nelkenrevolution vom 25. April 1974 führte zum Sturz des Regimes und eröffnete den Weg zur demokratischen Dritten Republik.</p>
 
-<p>Für lange Zeit war Portugal ein Auswanderungsland; wichtige Zentren der portugiesischen Kultur in der Diaspora gibt es heute in Frankreich und in den USA (wo jeweils etwa 1 bis 2 Millionen Portugiesen leben) sowie in Brasilien, Angola, Mosambik, der Schweiz und Luxemburg. Wichtigste bilaterale Partnerländer sind Brasilien und Spanien.</p>
+<h3>Atheer Huwaish</h3><br><br>
+<img class="info" src="../atheer_photo.jpg" alt="personal photo">
+<h4>Meine Webseiten (My Websites)</h4><br>
 
-<p>Der Tourismus ist eine wichtige Einnahmequelle. Mit 17 Millionen Touristen pro Jahr (2015[9]) gehört Portugal zu den meistbesuchten Ländern der Welt, häufigste Reiseziele sind die Algarve und die Region um die Hauptstadt Lissabon. Das Land ist daneben für den Weinanbau bekannt, insbesondere für den Portwein, und als weltweit bedeutendstes Produktionsland für Rohkork.</p>
-	</main>
+<h4>Responsives Template (2-spaltig)</h4>
+<a class="left" href="https://atheerhuwaish.github.io" target="_blank" rel="nofollow">2-spaltiges responsives Template (2-column responsive template)</a>.<br><br>
+
+<h4>Responsives Template (3-spaltig)</h4>
+<a class="left" href="https://atheerhuwaish.github.io/2" target="_blank" rel="nofollow">3-spaltiges responsives Template (3-column responsive template)</a>.<br><br>
+
+<h4>Wordpress eCommerce Webseite</h4>
+<a href="https://taharrur.com" target="_blank" rel="nofollow">Wordpress (Storefront & WooCommerce Website)</a>.<br><br>
+
+<a href="https://github.com/atheerhuwaish" target="_blank" rel="nofollow">Mein Github-Konto (my account on Github)</a>.<br><br>
+
+<h4>alfatrainging Weiterbildung - 28 Wochen (Courses - 28 Weeks)</h4>
+
+Zertifizierung - Certificates(Webmasters Europe)<br>
+-	Certified Web Designer<br>
+Note: 88%<br>
+-	Certified JavaScript Developer<br>
+Note: 81%<br>
+-	CMS - Wordpress und Joomla<br>
+-	PHP & MySQL<br>		
+-	Photoshop<br><br>					
+
+
+<h4>Sprachkenntnisse (Language skills)</h4>
+	Englisch<br>
+	Deutsch<br>
+	Arabisch<br>
+
+
+</main>
 
 	<?php include 'footer.php'; ?>
