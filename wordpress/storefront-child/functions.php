705078dfcecd85cacb06b66a8cb4d87b4ed027b6
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2019-10-29 13:04:33 +0100

    Create functions.php

---
+++ wordpress/storefront-child/functions.php
@@ -0,0 +1,85 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'storefront_enqueue_styles' );
+
+
+function storefront_enqueue_styles() {
+  
+ 
+    $parent_style = 'parent-style'; // This is 'storefront-style' for the Storefront theme.
+  
+  
+  
+    if ( is_rtl() ) {
+	
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style-rtl.css' );
+    
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style-rtl.css', array( $parent_style ), wp_get_theme()->get('Version')    ); }
+    
+    else
+    {
+  
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version')    ); 
+    
+	}  
+    
+}
+
+
+// beginning of code from tychesoftwares.com, Anubha Bhat to open external products in a new tab
+
+// // This will take care of the Buy Product button below the external product on the Shop page.
+add_filter( 'woocommerce_loop_add_to_cart_link', 'ts_external_add_product_link' , 10, 2 );
+
+// Remove the default WooCommerce external product Buy Product button on the individual Product page.
+remove_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+
+// Add the open in a new browser tab WooCommerce external product Buy Product button.
+add_action( 'woocommerce_external_add_to_cart', 'ts_external_add_to_cart', 30 );
+
+
+function ts_external_add_product_link( $link ) {
+global $product;
+
+if ( $product->is_type( 'external' ) ) {
+
+$link = sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s" target="_blank">%s</a>',
+esc_url( $product->add_to_cart_url() ),
+esc_attr( isset( $quantity ) ? $quantity : 1 ),
+esc_attr( $product->id ),
+esc_attr( $product->get_sku() ),
+esc_attr( isset( $class ) ? $class : 'button product_type_external' ),
+esc_html( $product->add_to_cart_text() )
+);
+}
+
+return $link;
+}
+
+function ts_external_add_to_cart() {
+global $product;
+
+if ( ! $product->add_to_cart_url() ) {
+return;
+}
+
+$product_url = $product->add_to_cart_url();
+$button_text = $product->single_add_to_cart_text();
+
+/**
+* The code below outputs the edited button with target="_blank" added to the html markup.
+*/
+do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+<p class="cart">
+<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt" target="_blank">
+<?php echo esc_html($button_text ); ?></a>
+</p>
+
+<?php do_action( 'woocommerce_after_add_to_cart_button' );
+
+}
+// end of code from tychesoftwares.com, Anubha Bhat
+
