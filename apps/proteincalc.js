6cf1f45a2e0256ab76daf27118177f3b5f3904d8
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2021-01-27 18:32:49 +0100

    Update proteincalc.js

--- apps/proteincalc.js
+++ apps/proteincalc.js
@@ -17,12 +17,16 @@ function proteincalculate (){
 	proteincontentelem.setAttribute("value", proteincontent);
 	
 	
-	food1.innerHTML += "<input type='number' class='foodamount' value='100' min='0' max='9999'> grams constitute ";
+	food1.innerHTML += "<input type='number' class='foodamount' value='100' min='0' max='9999'> grams for a person who weighs <input type='number' id='bodyweight' value='75' min='5' max='150'> constitute ";
+	var bodyweightelem = document.querySelector("#bodyweight");
+	var bodyweight = bodyweightelem.value;
+	bodyweightelem.setAttribute("value", bodyweight);
+	
 	var foodamountelem= document.querySelector(".foodamount");
 	var foodamount = foodamountelem.value;
 	foodamountelem.setAttribute("value", foodamount);
 	
-	proteinsubtotal= parseInt(foodamount * proteincontent / 70);
+	proteinsubtotal= parseInt(foodamount * proteincontent * .0008 * bodyweight );
 	food1.innerHTML += "<span>" + proteinsubtotal + "</span>";
 	
 	
