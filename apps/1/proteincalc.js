573110af2720c92a7ac7d17890247509a05b994a
atheerhuwaish <35437144+atheerhuwaish@users.noreply.github.com>
2021-02-02 21:59:14 +0100

    Update proteincalc.js

--- apps/1/proteincalc.js
+++ apps/1/proteincalc.js
@@ -107,7 +107,7 @@ function proteincalculate (){
 
 			
 			
-			var dailyrequirementingram = Math.ceil(factor * bodyweight);
+			var dailyrequirementingram = Math.round(factor * bodyweight);
 			
 			proteinsubtotal= Math.floor(foodamount * proteincontent / dailyrequirementingram);
 				
